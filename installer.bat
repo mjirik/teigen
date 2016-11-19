f072b8830ca1947a197d5426a28843e25790c99c
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-11-19 21:49:03 +0100

    windows installer added

---
+++ installer.bat
@@ -0,0 +1,11 @@
+set PATH=%PATH%;%HOMEPATH%\Miniconda2;%HOMEPATH%\Miniconda2\Scripts;C:\Miniconda2\Scripts;C:\Miniconda2
+set APPNAME=teigen
+conda create -y -c mjirik -c SimpleITK -c menpo --no-default-packages -n %APPNAME% pywget wget pip %APPNAME%
+
+call activate %APPNAME%
+rem :windows specific
+rem conda install -c jmargeta scikit-fmm
+
+rem python -m wget https://raw.githubusercontent.com/mjirik/lisa/master/requirements_pip.txt
+rem pip install -r requirements_pip.txt
+rem del requirements_pip.txt
