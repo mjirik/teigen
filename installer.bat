3c17e584b7bef2f9ce0b347dcfa91f962adaa07f
Miroslav Jirik <miroslav.jirik@gmail.com>
2016-11-21 12:43:41 +0100

    debugging install process

--- installer.bat
+++ installer.bat
@@ -1,10 +1,10 @@
 set PATH=%PATH%;%HOMEPATH%\Miniconda2;%HOMEPATH%\Miniconda2\Scripts;C:\Miniconda2\Scripts;C:\Miniconda2
 set APPNAME=teigen
-conda create -y -c mjirik -c SimpleITK -c menpo -c SimpleITK --no-default-packages -n %APPNAME% pywget wget pip %APPNAME%
+conda create -y -c mjirik -c SimpleITK -c menpo --no-default-packages -n %APPNAME% pywget wget pip %APPNAME%
 
 call activate %APPNAME%
 rem application acutalization if the installer is running for second time (and prev line fails because of existance of teigen environment)
-conda install -y -c mjirik %APPNAME%
+conda install -y -c mjirik -c SimpleITK %APPNAME%
 rem :windows specific
 rem conda install -c jmargeta scikit-fmm
 
