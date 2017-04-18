960f6f6654ad86095fc7457bb36c27d449e7537d
Miroslav Jirik <miroslav.jirik@gmail.com>
2017-04-18 11:51:39 +0200

    Bump version: 0.2.14 → 0.2.15

--- teigen/gui.py
+++ teigen/gui.py
@@ -635,7 +635,7 @@ class Teigen():
 
 
         self.logfile=logfile
-        self.version = "0.2.14"
+        self.version = "0.2.15"
         self.data3d = None
         self.voxelsize_mm = None
         self.need_run = True
